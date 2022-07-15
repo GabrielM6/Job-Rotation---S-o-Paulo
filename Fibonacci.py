018d7d888da65ece9be3d2aa9e74dd084333c003
GabrielM6 <85632644+GabrielM6@users.noreply.github.com>
2022-07-15 17:55:08 -0300

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,21 @@
+num = int(input('Digite o número:'))
+
+x = 0
+y = 1
+
+fibonacci = []
+fibonacci.append(x)
+fibonacci.append(y)
+for i in range(5):
+    x =  x + y
+    fibonacci.append(x)
+    y = x + y
+    fibonacci.append(y)
+
+if fibonacci.count(num) > 0:
+    print('O número informado pertence a sequencia fibonacci')
+else:
+    print('O número informado não pertence a sequencia fibonacci')
+
+
+print(fibonacci)
